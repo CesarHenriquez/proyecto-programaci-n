589c52fe1c6279d79391d82fbdb9b810dd0c7fe8
CesarHenriquez <ce.henriquezn@duocuc.cl>
2024-06-02 15:53:56 -0400

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -1,5 +1,9 @@
 #Proyecto Programación
-print("Estimado docente, que acción desea realizar?")
-print("""1. Promedio de notas
-         2. Gestión de asistencia del alumnado
-         3. Reporte de alumno con ropa informal""")
+accion_docente = int
+print("Estimado docente, que acción desea realizar?")      #borrador menu notas
+accion_docente=print(input("""1. Promedio de notas
+2. Gestión de asistencia del alumnado
+3. Reporte de alumno con ropa informal"""))
+if accion_docente == 1:
+   notas_alumno = print(input("Ingrese notas del alumnado"))
+      
