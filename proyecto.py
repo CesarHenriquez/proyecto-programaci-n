585fbf17dc261a93346c3734ed3b0504b27503a8
CesarHenriquez <ce.henriquezn@duocuc.cl>
2024-06-02 15:05:12 -0400

    Avance proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -1,2 +1,5 @@
 #Proyecto Programación
-
+print("Estimado docente, que acción desea realizar?")
+print("""1. Promedio de notas
+         2. Gestión de asistencia del alumnado
+         3. Reporte de alumno con ropa informal""")
