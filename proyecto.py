969a17cf6f5ed95ac241341fccb2291cca3ca407
CesarHenriquez <ce.henriquezn@duocuc.cl>
2024-06-06 09:58:43 -0400

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -5,6 +5,8 @@ print("           ¿Qué acción desea realizar?")
 accion_docente = int(input("""1. Promedio de notas    
 2. Asistencia e inasistencia
 3. Reporte de alumno con ropa informal
-4. Salir"""))
-if accion_docente == 1:
+4. Salir
+Ingrese una opción: """))
+while True:
+    if accion_docente == 1:
          notas = int(input("Registre las 8 notas del alumnado para saber su promedio:"))
